d6d78d6491014dbd8d57a56a664a860a7f75f1e5
SAISANKAR BOMMALA <saisankarbommala@gmail.com>
2026-02-04 21:18:27 +0530

    Update jobsApi.js

--- frontend/src/jobsApi.js
+++ frontend/src/jobsApi.js
@@ -4,7 +4,7 @@
 // API BASE URL
 // -------------------------------
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "https://qois.onrender.com/api";
 
 // -------------------------------
 // AUTH HEADERS
